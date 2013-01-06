f9d863e8d4e38501aa4320d8e17a2226e60122c3
Reid Draper <reiddraper@gmail.com>
2013-01-06 12:58:07 -0500

    Make Expression polymorphic, foldable

--- logic.hs
+++ logic.hs
@@ -1,5 +1,7 @@
 module Main where
 import Control.Monad (replicateM)
+import Data.Monoid
+import Data.Foldable (Foldable, foldMap)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
@@ -11,10 +13,9 @@ data UnaryOp = Not
 data BinaryOp = And | Or
 newtype Variable = Var Char deriving (Eq, Ord)
 
-data Expression = Bool Bool
-                | Variable Variable
-                | UnaryExpr (UnaryOp, Expression)
-                | BinaryExpr (BinaryOp, Expression, Expression)
+data Expression a = Leaf a
+                    | UnaryExpr (UnaryOp, Expression a)
+                    | BinaryExpr (BinaryOp, Expression a, Expression a)
 
 instance Show UnaryOp where
     show _ = "not"
@@ -26,41 +27,42 @@ instance Show BinaryOp where
 instance Show Variable where
     show (Var c) = [c]
 
-instance Show Expression where
-    show (Bool b) = show b
-    show (Variable v) = show v
+instance Show a => Show (Expression a) where
+    show (Leaf l) = show l
     show (UnaryExpr (op, expr)) = concat ["(", show op, " ", show expr, ")"]
     show (BinaryExpr (op, expr1, expr2)) = concat ["(", show expr1, " ",
                                                    show op, " ", show expr2, ")"]
 
+instance Foldable Expression where
+    foldMap f (Leaf v) = f v
+    foldMap f (UnaryExpr (_, expr)) = foldMap f expr
+    foldMap f (BinaryExpr (_, expr1, expr2)) = (foldMap f expr1) `mappend` (foldMap f expr2)
+
 type Mapping = Map.Map Variable Bool
+type VarExpr = Expression Variable
 
 ------------------------------------------------------------------------------
 -- Truth table
 ------------------------------------------------------------------------------
 
-truthTable :: Expression -> [Bool]
+truthTable :: VarExpr -> [Bool]
 truthTable expr = map (solveWithMapping expr) $ mappings expr
 
-solveWithMapping :: Expression -> Mapping -> Bool
+solveWithMapping :: VarExpr -> Mapping -> Bool
 solveWithMapping expr mapping = solve (replace expr mapping)
 
-replace :: Expression -> Mapping -> Expression
-replace s@(Bool _) _ = s
-replace (Variable v) m = Bool (Map.findWithDefault True v m)
+replace :: VarExpr -> Mapping -> Expression Bool
+replace (Leaf v) m = Leaf (Map.findWithDefault True v m)
 replace (UnaryExpr (op, expr)) m = UnaryExpr (op, replace expr m)
 replace (BinaryExpr (op, expr1, expr2)) m = BinaryExpr (op, (replace expr1 m), (replace expr2 m))
 
-mappings :: Expression -> [Mapping]
-mappings expr = let vs = Set.toList $ variables expr Set.empty
+mappings :: VarExpr -> [Mapping]
+mappings expr = let vs = Set.toList $ variables expr
                     tfs = replicateM (length vs) [True, False]
                 in map (Map.fromList . zip vs) tfs
 
-variables :: Expression -> Set.Set Variable -> Set.Set Variable
-variables (Bool _) s = s
-variables (Variable v) s = Set.insert v s
-variables (UnaryExpr (op, expr)) s = variables expr s
-variables (BinaryExpr (op, expr1, expr2)) s = Set.union (variables expr1 s) (variables expr2 s)
+variables :: VarExpr -> Set.Set Variable
+variables = foldMap Set.singleton
 
 ------------------------------------------------------------------------------
 -- Interpreter
@@ -73,8 +75,8 @@ operate_binary Or a b = a || b
 operate_unary :: UnaryOp -> Bool -> Bool
 operate_unary Not a = not a
 
-solve :: Expression -> Bool
-solve (Bool b) = b
+solve :: Expression Bool -> Bool
+solve (Leaf b) = b
 solve (UnaryExpr (operation, a)) = operate_unary operation (solve a)
 solve (BinaryExpr (operation, a, b)) = operate_binary operation (solve a) (solve b)
 
@@ -82,16 +84,16 @@ solve (BinaryExpr (operation, a, b)) = operate_binary operation (solve a) (solve
 -- Main
 ------------------------------------------------------------------------------
 
-expr1 :: Expression
-expr1 = BinaryExpr (Or, Variable (Var 'A'), (UnaryExpr (Not, Variable (Var 'B'))))
+expr1 :: VarExpr
+expr1 = BinaryExpr (Or, Leaf (Var 'A'), (UnaryExpr (Not, Leaf (Var 'B'))))
 
-expr2 :: Expression
-expr2 = BinaryExpr (And, Variable (Var 'A'), Variable (Var 'B'))
+expr2 :: VarExpr
+expr2 = BinaryExpr (And, Leaf (Var 'A'), Leaf (Var 'B'))
 
-expr3 :: Expression
+expr3 :: VarExpr
 expr3 = BinaryExpr (And, expr1, expr2)
 
-expr :: Expression
+expr :: VarExpr
 expr = UnaryExpr (Not, expr3)
 
 main :: IO ()
